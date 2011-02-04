5501c7d9b2458639532962b4b5659ba8f14f81fa
doug tangren <d.tangren@gmail.com>
2011-02-04 00:13:27 -0500

    the basics

--- project/build/Project.scala
+++ project/build/Project.scala
@@ -1,4 +1,4 @@
 import sbt._
-class Project(info: ProjectInfo) extends DefaultProject(info) {
+class Project(info: ProjectInfo) extends PluginProject(info) {
   val dbjs = "net.databinder" %% "dispatch-lift-json" % "0.7.8"
 }
--- src/main/scala/Issues.scala
+++ src/main/scala/Issues.scala
@@ -24,7 +24,96 @@ case class Issue(user: String, gravatar: String, updatedAt: String, votes: BigIn
                position: Double, title: String, body: String, state: String,
                createdAt: String)
 
-trait Issues {
+trait Issues extends sbt.Project with IssuesApi {
+
+  lazy val ghIssue = task {
+    _ match {
+      case Array(num) => issue(num.toLong) { _ match {
+        case Some(is) => task {
+          println("%s %s (@%s)\n%s" format(is.number, is.title, is.user, is.body))
+          None
+        }
+        case _ => task { Some("Github knows no issue %s" format num) }
+      } }
+      case _ => task { Some("usage: gh-issue <num>") }
+    }
+  } describedAs("Shows a github issue by number")
+
+  lazy val ghIssues = task {
+    issues { _ match {
+      case Nil => println("This project has no issues. At least no documented issues")
+      case l =>
+        println("open issues")
+        for(is <- l) println("%s %s (@%s)" format(is.number, is.user, is.title))
+    } }
+    None
+  } describedAs("Lists open github issues")
+
+  lazy val ghClosedIssues = task {
+     closedIssues { _ match {
+      case Nil => println("This project has no closed issues.")
+      case l =>
+        println("closed issues")
+        for(is <- l) println("%s %s (@%s)" format(is.number, is.title, is.user))
+    } }
+    None
+  } describedAs("List closed github issues")
+
+
+  lazy val ghSearchOpenIssues = task { _ match {
+    case Array() => task { Some("usage: gh-search-open-issues 'terms to search for'") }
+    case terms => task {
+      searchOpen(terms.mkString(" ")) { _ match {
+        case Nil => println("no open issues with the terms %s" format terms.mkString(" "))
+        case l =>
+          println("%s search results" format l.size)
+          for(is <- l) println("%s %s (@%s)" format(is.number, is.title, is.user))
+      } }
+      None
+    }
+  } } describedAs("Search for gh issues by terms")
+
+  lazy val ghSearchClosedIssues = task { _ match {
+    case Array() => task { Some("usage: gh-search-closed-issues 'terms to search for'") }
+    case terms => task {
+      searchClosed(terms.mkString(" ")) { _ match {
+        case Nil => println("no closed issues with the terms %s" format terms.mkString(" "))
+        case l =>
+          println("%s search results" format l.size)
+          for(is <- l) println("%s %s (@%s)" format(is.number, is.title, is.user))
+      } }
+      None
+    }
+  } } describedAs("Search for gh issues by terms")
+
+  lazy val ghOpen = task { _ match {
+    case Array(title, desc) =>
+      openIssue(title, desc) { _ match {
+        case Some(is) => task {
+          println("created issue %s %s (@%s)" format(is.number, is.title, is.user))
+          None
+        }
+        case _ => task { Some("error creating issue") }
+      } }
+    case _ => task { Some("usage: gh-open '<title>' '<description>'") }
+  } } describedAs("Open gh issue")
+
+
+ lazy val ghClose = task { _ match {
+    case Array(num) =>
+      closeIssue(num.toLong) { _ match {
+        case Some(is) => task {
+          println("closed issue %s %s (@%s)" format(is.number, is.title, is.user))
+          None
+        }
+        case _ => task { Some("error creating issue") }
+      } }
+    case _ => task { Some("usage: gh-close <num>") }
+  } } describedAs("Close gh issue")
+
+}
+
+private [gh] trait IssuesApi {
   import dispatch._
   import dispatch.Http._
   import dispatch.liftjson.Js._
@@ -32,19 +121,23 @@ trait Issues {
   import net.liftweb.json.JsonAST._
   import java.net.URLEncoder
 
-  val github = :/("github.com") / "api" / "v2" / "json" / "issues"
+  private [gh] val github = :/("github.com") / "api" / "v2" / "json" / "issues"
 
-  def ghUser: String
+  /** (username, reponame) */
+  def ghRepository: (String, String)
 
-  def ghRepo: String
+  lazy val ghRepo = ghRepository._2
 
+  lazy val ghUser = ghRepository._1
+
+  /** (username, password) */
   def ghCredentials: (String, String)
 
   lazy val auth = ghCredentials
 
   def issue[T](num: Long)(fn:  Option[Issue] => T) =
     Http(github / "show" / ghUser / ghRepo / num.toString ># { js =>
-      fn(oneIssue(js).headOption)
+      fn(oneIssue(js))
     })
 
   def issues[T](fn: List[Issue] => T) =
@@ -58,7 +151,7 @@ trait Issues {
     })
 
   def searchOpen[T](term: String)(f: List[Issue] => T) =
-    Http(github / "search" / ghUser / ghRepo / "closed" / URLEncoder.encode(term, "utf-8") ># { js =>
+    Http(github / "search" / ghUser / ghRepo / "open" / URLEncoder.encode(term, "utf-8") ># { js =>
       f(manyIssues(js))
     })
 
@@ -67,19 +160,19 @@ trait Issues {
       f(manyIssues(js))
     })
 
-  def openIssue[T](title: String, body: String)(f: Any => T) = {
+  def openIssue[T](title: String, body: String)(f: Option[Issue] => T) = {
     val (user, pass) = auth
     Http(github.POST.as_!(user, pass) / "open" / ghUser / ghRepo << Map(
       "title" -> title, "body" -> body
     ) ># { js =>
-      f(js)
+      f(oneIssue(js))
     })
   }
 
-  def closeIssue[T](num: Long)(f: Any => T) = {
+  def closeIssue[T](num: Long)(f: Option[Issue] => T) = {
     val (user, pass) = auth
     Http(github.POST.as_!(user, pass) / "close" / ghUser / ghRepo / num.toString ># { js =>
-      f(js)
+      f(oneIssue(js))
     })
   }
 
@@ -102,8 +195,8 @@ trait Issues {
     })
   }
 
-  def oneIssue(js: JValue) =
-    for {
+  def oneIssue(js: JValue): Option[Issue] = try {
+    (for {
       f <- Issues.one(js)
       grav <- Issues.gravatar(f)
       position <- Issues.position(f)
@@ -117,9 +210,10 @@ trait Issues {
       number   <- Issues.number(f)
       } yield {
         Issue(user, grav, updated, votes, number, position, title, body, state, created)
-      }
+      }).headOption
+  } catch { case dispatch.StatusCode(c,_) => None }
 
- def manyIssues(js: JValue) =
+ def manyIssues(js: JValue): List[Issue] = try {
    for {
      is <- Issues.many(js)
      f <- is
@@ -135,5 +229,5 @@ trait Issues {
      number   <- Issues.number(f)
    } yield {
      Issue(user, grav, updated, votes, number, position, title, body, state, created)
-   }
+   } } catch { case dispatch.StatusCode(c,_) => Nil }
 }
